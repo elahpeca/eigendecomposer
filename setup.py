8f6391ca4adce1a38e2fce05ac83a18f0d61a707
elahpeca <acephaleee@gmail.com>
2024-07-25 20:44:20 +0300

    Changed version to 1.1-a

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
        name='eigen-decomposer',
-       version='0.1.0',
+       version='1.1-a',
        description='A Python script for eigenvalue decomposition and visualization',
        author='elahpeca',
        author_email='acephaleee@gmail.com', 
