e3896b5c28cf3cb4c8e0c5a26792a3242380acb0
elahpeca <acephaleee@gmail.com>
2024-07-25 20:35:09 +0300

    Added rounder to the eigendecomposer algorithm

--- eigendecomposer.py
+++ eigendecomposer.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 from matrix_utils import size_getter, matrix_getter
-from eigencalculations import eigencalculator
+from eigencalculations import eigencalculator, rounder
 from plot_utils import plot_decomposer
 
 def eigendecomposer(matrix):
     """Performs eigenvalue decomposition and visualizes the results."""
 
     v, d, v_inv, matrix_rec = eigencalculator(matrix)
+    v, d, v_inv, matrix_rec = rounder(v, d, v_inv, matrix_rec)
     plot_decomposer(v, d, v_inv, matrix_rec)
 
 
