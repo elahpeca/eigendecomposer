feb43803641bba5017cc42a98f4421853bcb38ac
elahpeca <acephaleee@gmail.com>
2024-07-25 23:51:32 +0300

    Fixed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
        name='eigendecomposer',
-       version='1.1-a',
+       version='1.1.0a',
        description='A Python script for eigenvalue decomposition and visualization',
        author='elahpeca',
        author_email='acephaleee@gmail.com', 
